52382ac68eab1c180531631fb71b1b8d05d28e1d
IMYdev <omranmansour096@gmail.com>
2024-01-19 15:36:53 +0200

    Initial commit

---
+++ twrp_lion.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit from kat device
+$(call inherit-product, device/infinix/kat/device.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := kat
+PRODUCT_NAME := twrp_kat
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Note 12
+PRODUCT_MANUFACTURER := infinix
